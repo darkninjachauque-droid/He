33a2dc9e381ff8dd589e20b352fb11d1613849d8
TES APK NET <nettesapk@gmail.com>
2026-02-15 10:27:20 +0000

    Qual é a ideia agora mesmo sendo das regras dele mesmo se ali mostrar he

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,7 +3,6 @@
 
 export const firebaseConfig = {
   apiKey: "AIzaSyBso-PWibFZmgxAAH26DQxUinlBjjD5p-g",
-  // Usando o domínio da Netlify para que ele apareça na tela de login do Google
   authDomain: "heliotech-arquivo-seguro.netlify.app",
   projectId: "studio-9995869127-3ea67",
   storageBucket: "studio-9995869127-3ea67.firebasestorage.app",
